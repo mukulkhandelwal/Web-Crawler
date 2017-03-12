43f8bc770c16edb872d1d377c11fb4b114a19051
Mukul <you@example.com>
2017-03-12 21:20:25 +0530

    queue and crawled files

--- main.py
+++ main.py
@@ -6,8 +6,28 @@ def create_project_dir(directory):
 		print("Creating project " + directory)
 		os.makedirs(directory)
 
+ 
+#create_project_dir("facebook")
 
-create_project_dir("facebook")
+#queue and crawled files
+
+#create queur and crawled files( if not created)
+def create_data_files(project_name, base_url):
+	queue = project_name + 'queue.txt'
+	crawled = project_name + 'crawled.txt'
+	if not os.path.isfile(queue):
+		write_file(queue, base_url)
+	if not os.path.isfile(crawled):
+		write_file(crawled,'')
+
+
+#Create a new file
+def write_file(path, data):
+	f = open(path,'w')
+	f.write(data)
+	f.close()
+
+
 
 
 
