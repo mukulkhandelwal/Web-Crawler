0fa133eaa64d1fa964f498d000fcc019d4f52c04
Mukul <you@example.com>
2017-03-13 11:13:35 +0530

    parsing html

---
+++ link_finder.py
@@ -0,0 +1,21 @@
+#parsing html
+
+from HTMLParser import HTMLParser
+from urlparse import urlparse
+
+class LinkFinder(HTMLParser):
+
+
+	def __init__(self):
+		super().__init__()
+
+	def handle_starttag(self, tag, attrs):
+		print(tag)
+
+	def error(self, message):
+		pass
+
+
+finder = LinkFinder()
+finder.feed('<html><head><title>Test</title></head'
+			'<body><h1>Parse me!</h1></bodt></html>')
