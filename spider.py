88d84d1f4c061e4160194bec28bcf33a7cd180fe
Mukul <you@example.com>
2017-03-13 16:32:41 +0530

    creating the spider

--- spider.py
+++ spider.py
@@ -1,1 +1,27 @@
+from urllib.request import urlopen #connect webpages in python
+from link_finder import LinkFinder
+from general import *
+
 class Spider:
+
+	# class variables (shared among all instances(spides)
+	project_name = ''
+	base_url = ''
+	domain_name = ''
+	queue_file = ''  #text file .. save the data
+	crawled_file = ''
+	queue = set() #waiting list dont want to write every time to file
+	crawled = set()
+
+
+	def __init__(self):
+
+
+
+
+
+
+
+
+
+
