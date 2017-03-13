20bed6486c499fe0f3d67f85084dfe5a4df6e66d
Mukul <you@example.com>
2017-03-13 20:56:57 +0530

    gathering links:

--- spider.py
+++ spider.py
@@ -36,12 +36,28 @@ class Spider:
 		if page_url not in Spider.crawled:
 			print("Thread name" + thread_name + "now crawling "+ page_url)
 			print('Queue ' + str(len(Spider.queue)) + " | Creawled "+ str(len(Spider.crawled)))
-			Spider.add_links_to_queue(Spider.gather_link(page_url)) #set of all links in web page
+			Spider.add_links_to_queue(Spider.gather_links(page_url)) #set of all links in web page
 			Spider.queue.remove(page_url)
 			Spider.crawled.add(page_url)
 			Spider.update_files()
 
 
+	@staticmethod
+	def gather_links(page_url):
+		html_string = ''
+		try:
+			response = urlopen(page_url) #return byte data
+			if response.getheader('Content-Type') = 'text/html':
+				html_bytes = response.read() #1s and 0s
+				html_string = html_bytes.decode("utf-8") #convert to string te binary web data
+
+			finder = LinkFinder(Spider.base_url,page_url)
+			finder.feed(html_string)
+		except:
+			print("Error : can not crawled page")
+			return set()
+
+		return finder.page_links()
 
 
 
