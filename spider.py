005f3486880568a8d9f5b190f2ee13bff3e3ec77
Mukul <you@example.com>
2017-03-13 20:38:37 +0530

    Crawling pages

--- spider.py
+++ spider.py
@@ -29,7 +29,17 @@ class Spider:
 		create_project_dir(Spider.project_name)
 		create_data_files(Spider.project_name, Spider.base_url)
 		Spider.queue = file_to_set(Spider.queue_file)
-		Spider.crawled = file_to_set(Spider.crawled_file)		
+		Spider.crawled = file_to_set(Spider.crawled_file)
+
+	@staticmethod
+	def crawl_page(thread_name, page_url):
+		if page_url not in Spider.crawled:
+			print("Thread name" + thread_name + "now crawling "+ page_url)
+			print('Queue ' + str(len(Spider.queue)) + " | Creawled "+ str(len(Spider.crawled)))
+			Spider.add_links_to_queue(Spider.gather_link(page_url)) #set of all links in web page
+			Spider.queue.remove(page_url)
+			Spider.crawled.add(page_url)
+			Spider.update_files()
 
 
 
